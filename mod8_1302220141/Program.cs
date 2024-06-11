fd8075ab672950a7cd5db45d3bc8716bf95ad9fa
unknown <tsaqifzayyan123@gmail.com>
2024-06-11 13:22:41 +0700

    aplikasi bank transfer dengan runtime configuration

--- mod8_1302220141/Program.cs
+++ mod8_1302220141/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace program;
+using mod8_1302220141.UIconfig;
+using Newtonsoft.Json;
+using mod8_1302220141.BankTransferConfig;
+class Program
+{
+    static void Main()
+    {
+        /* String path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName ;
+         String jsonString = File.ReadAllText(path + "//bank_transfer_config.json");
+         BankTransferConfig config = JsonConvert.DeserializeObject<BankTransferConfig>(jsonString);
+         Console.WriteLine("Language: " + config.lang);
+
+         jsonString = JsonConvert.SerializeObject(config);
+         File.WriteAllText(path + "//bank_transfer_config.json", jsonString);*/
+
+        UIConfig uiConfig = new UIConfig();
+        uiConfig.addConfig();
+    }
+}
